8eadeb658af52b865d564ec802859da380c20e86
ex61wi <tim.rorije@ing.com>
2025-03-16 12:16:15 +0100

    wip

--- DcsTypes.lua
+++ DcsTypes.lua
@@ -20,7 +20,16 @@
 ---@field y number directed to the east 
 ---@field z number directed up (away from ground)
 
----@class Array<T>: { [integer]: T } 
+---@class Position : Vec3
+---@field p Vec3 Point on map
+
+---@class Array<T>: { [number]: T } 
+
+---@class MGRS
+---@field UTMZone string
+---@field MGRSDigraph string
+---@field Easting number
+---@field Northing number
 
 
 do -- env
@@ -270,11 +279,11 @@ do -- coalition
         
         ---@class MissionStaticObjectTable
         ---@field heading number
-        ---@field groupId integer
+        ---@field groupId number
         ---@field shape_name string
         ---@field type string
-        ---@field unitId integer
-        ---@field rate integer
+        ---@field unitId number
+        ---@field rate number
         ---@field name string
         ---@field category string
         ---@field x number
@@ -312,7 +321,7 @@ do -- trigger
     ---@field getZone fun(zoneName: string) : Array<TriggerZone> returns triggerzones. Only works for cilinders really. For more detailed zone data see: env.mission.triggers.zones
     trigger.misc = trigger.misc
 
-    ---@class TriggerActions : OutText, OutSound, OtherCommands, MarkCommands
+    ---@class TriggerActions : OutText, OutSound, OtherCommands, MarkCommands, AITriggerCommands
     ---@field ctfColorTag fun(unitName: string, smokeColor: SmokePlumeColor, minAlt: number?) Created a smoke plume behind a specified aircraft
     ---@field setUserFlag fun(flagName: string, userFlagValue: boolean|number) Sets a user flag to a specified value
     ---@field explosion fun(point: Vec3, power: number) Creates an explosion at a given point at the specified power. 
@@ -330,46 +339,54 @@ do -- trigger
     ---@field outSound fun(fileName: string) Plays a sound file to all players.
     ---@field outSoundForCoalition fun(coalitionSide: CoalitionSide, soundFile: string) Plays a sound file to all players on the specified coalition. 
     ---@field outSoundForCountry fun(country: CountryID, soundfile: string) Plays a sound file to all players on the specified country.
-    ---@field outSoundForGroup fun(groupId: integer, soundFile: string) Plays a sound file to all players in the specified group.
-    ---@field outSoundForUnit fun(unitId: integer, soundFile: string) Plays a sound file to all players in the specified unit.
+    ---@field outSoundForGroup fun(groupId: number, soundFile: string) Plays a sound file to all players in the specified group.
+    ---@field outSoundForUnit fun(unitId: number, soundFile: string) Plays a sound file to all players in the specified unit.
 
     ---@class OutText
     ---@field outText fun(text: string, displayTime: number, clearview: boolean?) Displays the passed string of text for the specified time to all players.
     ---@field outTextForCoalition fun(coalitionId: CoalitionSide, text: string, displayTime: number, clearview: boolean?) Displays the passed string of text for the specified time to all players belonging to the specified coalition.
     ---@field outTextForCountry fun(country: CountryID, text: string, displayTime: number, clearview: boolean?) Displays the passed string of text for the specified time to all players belonging to the specified country.
-    ---@field outTextForGroup fun(groupId: integer, text: string, displayTime: number, clearView: boolean?) Displays the passed string of text for the specified time to all players in the specified group.
-    ---@field outTextForUnit fun(unitId: integer, text: string, displayTime: number, clearView: boolean?) Displays the passed string of text for the specified time to all players in the specified unit.
+    ---@field outTextForGroup fun(groupId: number, text: string, displayTime: number, clearView: boolean?) Displays the passed string of text for the specified time to all players in the specified group.
+    ---@field outTextForUnit fun(unitId: number, text: string, displayTime: number, clearView: boolean?) Displays the passed string of text for the specified time to all players in the specified unit.
 
     ---@class OtherCommands
     ---@field addOtherCommand fun(name: string, userFlagName: string, userFlagValue: string) Adds a command to the "F10 Other" radio menu allowing players to call commands and set flags within the mission.
     ---@field removeOtherCommand fun(name: string) Removes the command that matches the specified name input variable from the "F10 Other" radio menu. 
     ---@field addOtherCommandForCoalition fun(coalitionId: CoalitionSide, name: string, userFlagName: string, userFlagValue: number) Adds a command to the "F10 Other" radio menu allowing players to call commands and set flags within the mission.
     ---@field removeOtherCommandForCoalition fun(coalitionId: CoalitionSide, name: string) Removes the command that matches the specified name input variable from the "F10 Other" radio menu if the command was added for coalition.
-    ---@field addOtherCommandForGroup  fun(groupId: integer, name: string, userFlagName: string, userFlagValue: number) Adds a command to the "F10 Other" radio menu allowing players to call commands and set flags within the mission.
-    ---@field removeOtherCommandForGroup fun(groupId: integer, name: string) Removes the command that matches the specified name input variable from the "F10 Other" radio menu if the command exists for the specified group. 
+    ---@field addOtherCommandForGroup  fun(groupId: number, name: string, userFlagName: string, userFlagValue: number) Adds a command to the "F10 Other" radio menu allowing players to call commands and set flags within the mission.
+    ---@field removeOtherCommandForGroup fun(groupId: number, name: string) Removes the command that matches the specified name input variable from the "F10 Other" radio menu if the command exists for the specified group. 
 
     ---@class MarkCommands 
-    ---@field markToAll fun(id: integer, text:string, point:Vec3, readOnly: boolean?, message: string?) Adds a mark point to all on the F10 map with attached text. 
-    ---@field markToCoalition fun(id: integer, text: string, point: Vec3, coalitionID: CoalitionSide, readOnly: boolean?, message: string) Adds a mark point to a coalition on the F10 map with attached text. 
-    ---@field markToGroup fun(id: integer, text: string, point: Vec3, groupID: integer, readOnly: boolean?, message: string) Adds a mark point to a group on the F10 map with attached text. 
-    ---@field removeMark fun(id: integer) Removes a mark panel from the f10 map 
-    ---@field markupToAll fun(shapeID: ShapeId, coalition: DrawCoalition, id: integer, ... : Vec3, lineColor: table, fillColor: table, lineType: LineType)
-    ---@field lineToAll
-    ---@field circleToAll
-    ---@field rectToAll
-    ---@field quadToAll
-    ---@field textToAll
-    ---@field arrowToAll
-    ---@field setMarkupRadius
-    ---@field setMarkupText
-    ---@field setMarkupFontSize
-    ---@field setMarkupColor
-    ---@field setMarkupColorFill
-    ---@field setMarkupTypeLine
-    ---@field setMarkupPositionEnd
-    ---@field setMarkupPositionStart 
+    ---@field markToAll fun(id: number, text:string, point:Vec3, readOnly: boolean?, message: string?) Adds a mark point to all on the F10 map with attached text. 
+    ---@field markToCoalition fun(id: number, text: string, point: Vec3, coalitionID: CoalitionSide, readOnly: boolean?, message: string) Adds a mark point to a coalition on the F10 map with attached text. 
+    ---@field markToGroup fun(id: number, text: string, point: Vec3, groupID: number, readOnly: boolean?, message: string) Adds a mark point to a group on the F10 map with attached text. 
+    ---@field removeMark fun(id: number) Removes a mark panel from the f10 map 
+    ---@field markupToAll fun(shapeID: ShapeId, coalition: DrawCoalition, id: number, ... : any) Complex parameters.  <br/> See: https://wiki.hoggitworld.com/view/DCS_func_markupToAll
+    ---@field lineToAll fun(coalition: DrawCoalition, id: number, startPoint:Vec3, endPoint: Vec3, color:table, lineType: LineType, readonly: boolean?, message: string?) Creates a line on the F10 map from one point to another. 
+    ---@field circleToAll fun(coalition: DrawCoalition, id: number, center: Vec3 , radius: number, color: table , fillColor: table , lineType: LineType , readOnly: boolean?, message: string?) Creates a circle on the map with a given radius, color, fill color, and outline. 
+    ---@field rectToAll fun(coalition:DrawCoalition, id:number, startPoint: Vec3, endPoint:Vec3, color: table, fillColor: table, lineType: LineType , readOnly: boolean?, message: string?)  	Creates a rectangle on the map from the startpoint in one corner to the endPoint in the opposite corner. 
+    ---@field quadToAll fun(coalition:DrawCoalition, id:number, point1: Vec3, point2:Vec3, point3:Vec3, point4:Vec3,color: table, fillColor: table, lineType: LineType , readOnly: boolean?, message: string?) Creates a shape defined by the 4 points on the F10 map. 
+    ---@field textToAll fun(coalition:DrawCoalition, id: number, point:Vec3, color: table, fillColor: table, fontSize:table, readOnly:boolean, text:string) Creates a text imposed on the map at a given point. Text scales with the map. 
+    ---@field arrowToAll fun(coalition:DrawCoalition, id: number, startPoint:Vec3, endPoint:Vec3, color:table, fillColor:table, lineType:LineType, readonly:boolean?, message: string?) Creates an arrow from the startPoint to the endPoint on the F10 map. The arrow will be "pointing at" the startPoint
+    ---@field setMarkupRadius fun(id:number, radius:number) Updates the radius of the specified mark to be the new value.
+    ---@field setMarkupText fun(id:number, text: string) Updates the text value of the passed mark to the passed text value. 
+    ---@field setMarkupFontSize fun(id:number, fontSize:number) Updates the font size of the specified mark to be the new value.
+    ---@field setMarkupColor fun(id:number, color:table) Updates the color of the specified mark to be the new value. 
+    ---@field setMarkupColorFill fun(id: number, color: table) Updates the fill color of the specified mark to be the new value. 
+    ---@field setMarkupTypeLine fun(id:number, lineType: LineType) Updates the type line of the specified mark to be the new value. 
+    ---@field setMarkupPositionEnd fun(id: number, end:Vec3) Updates the position of a mark that was defined at the last point given to create it.
+    ---@field setMarkupPositionStart fun(id: number, end:Vec3) Updates the position of a mark that was defined at the last point given to create it.
 
-    trigger.action.markupToAll()
+    ---@class AITriggerCommands
+    ---@field setAITask fun(group:Group, taskIndex:number) Sets the task of the specified index to be the one and only active task. 
+    ---@field pushAITask fun(group:Group, taskIndex:number) Pushes the task of the specified index to the front of the tasking queue. 
+    ---@field activateGroup fun(group:Group) Activates the specified group if it is setup for "late activation." Calls the Group.activate function. 
+    ---@field deactivateGroup fun(group:Group) Deactivates the specified group. Calls the Group.destroy function.
+    ---@field setGroupAIOn fun(group:Group) Turns the specified groups AI on. Calls the Group.getController(setOnOff(true)) function. 
+    ---@field setGroupAIOff fun(group:Group) Turns the specified groups AI off. Calls the Group.getController(setOnOff(false)) function. 
+    ---@field groupStopMoving fun(group:Group) Orders the specified group to stop moving. Calls Group.getController(setCommand()) function and sets the stopRoute command to true. 
+    ---@field groupContinueMoving fun(group:Group) Orders the specified group to resume moving. Calls Group.getController(setCommand()) function and sets the stopRoute command to false. 
 
     ---@enum SmokeColor
     trigger.smokeColor = {
@@ -440,9 +457,110 @@ do -- trigger
 
 end
 
----@class Object
----@field getName fun(self) : string
-Object = Object
+do --coord
+
+---@class Coord
+---@field LLtoLO fun(lattitude: number, longitude:number, altitude: number?):Vec3 Returns a point from latitude and longitude in the vec3 format. 
+---@field LOtoLL fun(point:Vec3): lattitude:number, longitude:number, altitude:number Returns multiple values of a given vec3 point in latitude, longitude, and altitude 
+---@field LLtoMGRS fun(lattitude: number, longitude:number):MGRS Returns an MGRS table from the latitude and longitude coordinates provided
+---@field MGRStoLL fun(mgrs:MGRS): lattitude:number, longitude:number, altitude:number Returns multiple values of a given in MGRS coordinates and converts it to latitude, longitude, and altitude 
+coord = coord
+
+end
+
+do -- missioncommands
+
+    ---@class MissionCommands
+    ---@field addCommand fun(name:string, path: table?, functionToRun: function, argument: any?):table Adds a command to the "F10 Other" radio menu allowing players to run specified scripting functions.
+    ---@field addSubMenu fun(name: string, path: table?): table Creates a submenu of a specified name for all players.
+    ---@field removeItem fun(path:table?) Removes the item of the specified path from the F10 radio menu for all.
+    ---@field addCommandForCoalition fun(coalition: CoalitionSide, name: string, path: table?, functionToRun:function, argument:any?):table Adds a command to the "F10 Other" radio menu allowing players to run specified scripting functions. 
+    ---@field addSubMenuForCoalition fun(coalition:CoalitionSide, name: string, path:table?):table Creates a submenu of a specified name for the specified coalition. Can be used to create nested sub menues. If the path is not specified, submenu is added to the root menu.
+    ---@field removeItemForCoalition fun(coalition:CoalitionSide, path:table?) Removes the item of the specified path from the F10 radio menu for the specified coalition.
+    ---@field addCommandForGroup fun(groupId: number, name: string, path: table?, functionToRun:function, argument:any?):table Adds a command to the "F10 Other" radio menu allowing players to run specified scripting functions. 
+    ---@field addSubMenuForGroup fun(groupId: number, name:string, path:table?): table Creates a submenu of a specified name for the specified group.
+    ---@field removeItemForGroup fun(groupId: number, path:table?) Removes the item of the specified path from the F10 radio menu for the specified group.
+    missionCommands = missionCommands
+
+end
+
+do -- net
+
+    ---@class Net
+    ---@field send_chat fun(message:string, all:boolean?) Sends a chat message. 
+    ---@field send_chat_to fun(message: string, playerID: number, fromID: number?) Sends a chat message to the player with the passed id. 
+    ---@field load_mission fun(name:string):boolean Loads the specified mission.
+    ---@field load_next_mission fun():boolean Load the next mission from the server mission list.
+    ---@field get_player_list fun():table? Returns players currently connected TODO: Document table
+    ---@field get_my_player_id fun():number returns playerID
+    ---@field get_server_id fun():number returns the playerID for the server (currently always 1)
+    ---@field get_player_info fun(playerID: number, attribute? : string) : PlayerInfo Returns a table of attributes for a given playerId. If optional attribute present only that value is returned
+    ---@field kick fun(playerID:number, message:string?):boolean Kicks a player from the server. Can display a message to the user.
+    ---@field get_stat fun(playeID:number, statID:number?):number
+    ---@field get_name fun(playerID:number):string get players name
+    ---@field get_slot fun(playerID:number):coalitionID:CoalitionSide,slotID:number gets coalitionID and slotID of a player
+    ---@field force_player_slot fun(playerID: number, sideID: CoalitionSide, slotID:number) forces a  
+    ---@field lua2json fun(luaObject:any):string converts a lua object to a json string
+    ---@field json2lua fun(json: string):table converts a json string to a lua object
+    ---@field dostring_in fun(state:string, dostring:string):string Executes a lua string in a given lua environment in the game. 
+    ---@field log fun(text:string) writes INFO log to, recommend using "env" singleton to log as it supports all levels.
+    net = net
+
+    ---@class PlayerInfo
+    ---@field id number
+    ---@field name string
+    ---@field side CoalitionSide
+    ---@field slotID number
+    ---@field ping number
+    ---@field ipaddr string
+    ---@field ucid string
+
+    -- undocumented
+    --@field set_slot
+    --@field recv_chat 
+end
+
+--========================
+-- Classes
+--========================
+
+
+do -- Object
+
+    ---@class Object
+    ---@field getName fun(self:Object): string returns the name of the object in the mission environment
+    ---@field isExist fun(self:Object): boolean Return a boolean value based on whether the object currently exists in the mission. 
+    ---@field destroy fun(self:Object) Destroys the object, physically removing it from the game world without creating an event.
+    ---@field getCategory fun(self:Object): category: ObjectCategory, subCategory:number Returns an enumerator of the category for the specific object. <br/>!!Dependent on how called. Check docs
+    ---@field getTypeName fun(self:Object): string returns the typename of the object
+    ---@field getDesc fun(self:Object):table returns description table, which is different for each type and subtype and even object
+    ---@field hasAttribute fun(self:Object, attribute:string):boolean Returns a boolean value if the object in question has the passed attribute.
+    ---@field getPoint fun(self:Object):Vec3 Returns a vec3 table of the x, y, and z coordinates for the position of the given object in 3D space.
+    ---@field getPosition fun(self:Object):Position Returns a Position3 table of the objects current position and orientation in 3D space. 
+    ---@field getVelocity fun(self:Object):Vec3 Returns a vec3 table of the objects velocity vectors.
+    ---@field inAir fun(self:Object):boolean Returns a boolean value if the object in question is in the air.
+    Object = Object
+
+    ---@enum ObjectCategory
+    Object.Category = {
+        UNIT = 1,
+        WEAPON = 2,
+        STATIC = 3,
+        BASE = 4,
+        SCENERY = 5,
+        Cargo = 6
+    }
+
+end
+
+do --SceneryObject
+
+    ---@class SceneryObject
+    ---@field getLife fun(self:SceneryObject):number gets the life of a sceneryObject
+    ---@field getDescByName fun(typeName:string):table gets the description based on the typename
+    SceneryObject = SceneryObject
+
+end
 
 ---@class CoalitionObject
 
@@ -452,15 +570,6 @@ Object = Object
 
 
 
----@enum ObjectCategory
-Object.Category = {
-    UNIT = 1,
-    WEAPON = 2,
-    STATIC = 3,
-    BASE = 4,
-    SCENERY = 5,
-    Cargo = 6
-}
 
 
 country = country
